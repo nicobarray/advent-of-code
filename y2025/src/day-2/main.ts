7e72d9ab08fffd3fa4424979b393ce6250fe9fea
Nicolas Barray <nicolas.barray@amblea.fr>
2025-12-06 11:38:12 +0100

    add 2025-day2, part2

--- y2025/src/day-2/main.ts
+++ y2025/src/day-2/main.ts
@@ -2,27 +2,59 @@ import { readDayInputs } from "../lib/utils.ts";
 
 function solve(input: string) {
   let part1 = 0;
-  // let part2 = 0;
+  let part2 = 0;
 
   for (const range of input.split(",")) {
     const [from, to] = range.split("-").map((v) => parseInt(v));
 
     for (let i = from; i <= to; i++) {
-      const j = i.toString();
-      const l = j.substring(0, j.length / 2);
-      const r = j.substring(j.length / 2);
-      if (l === r) {
-        console.log(j, l, r);
+      const id = i.toString();
+      const left = id.substring(0, id.length / 2);
+      const right = id.substring(id.length / 2);
+      if (left === right) {
+        // console.log("[part1]", id, left, right);
         part1 += i;
+      }
+
+      let isInvalidId = false;
+      for (let k = 1; k <= id.length / 2; k++) {
+        const pattern = id.substring(0, k);
+        for (
+          let times = 1;
+          times <= id.length / pattern.length;
+          times++
+        ) {
+          if (id === pattern.repeat(times)) {
+            // console.log(
+            //   "[part2]",
+            //   "range is [" + from + "," + to + "]",
+            //   id,
+            //   "is just",
+            //   pattern,
+            //   "repeated",
+            //   times,
+            //   "times",
+            // );
+            part2 += i;
+            isInvalidId = true;
+            break;
+          }
+        }
+
+        if (isInvalidId) {
+          break;
+        }
       }
     }
   }
 
   console.log("Anwser 1:", part1);
-  // console.log("Anwser 2:", part2);
+  console.log("Anwser 2:", part2);
 }
 
 const [example, input] = await readDayInputs(import.meta.dirname);
 
+console.log("Example");
+solve(example);
+console.log("Input");
 solve(input);
-// solve(example);
